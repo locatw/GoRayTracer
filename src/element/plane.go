b87891f0f4916c8f0acfadf659d3df3af2c22dec
locatw <locatw@users.noreply.github.com>
2019-02-27 23:37:02 +0900

    add Plane

---
+++ src/element/plane.go
@@ -0,0 +1,31 @@
+package element
+
+import . "../vector"
+
+type Plane struct {
+	Center   Vector
+	Normal   Vector
+	Material Material
+}
+
+func (plane *Plane) Intersect(ray Ray) *HitInfo {
+	a := Dot(ray.Direction, plane.Normal)
+
+	if a == 0.0 {
+		return nil
+	}
+
+	b := Dot(Subtract(plane.Center, ray.Origin), plane.Normal)
+	t := b / a
+	if 0.0 < t {
+		pos := Add(Multiply(t, ray.Direction), ray.Origin)
+
+		return &HitInfo{Object: plane, Position: pos, Normal: plane.Normal, T: t}
+	} else {
+		return nil
+	}
+}
+
+func (plane *Plane) GetMaterial() Material {
+	return plane.Material
+}
