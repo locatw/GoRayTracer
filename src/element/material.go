2cf8f3e8c664d050dddd48cdfbe6356d394c5885
locatw <locatw@users.noreply.github.com>
2019-02-28 23:27:06 +0900

    add a function which creates default material

--- src/element/material.go
+++ src/element/material.go
@@ -6,3 +6,9 @@ type Material struct {
 	Emission Color
 	Diffuse  Color
 }
+
+func CreateDefaultMaterial() Material {
+	black := CreateDefaultColor(Black)
+
+	return Material{Emission: black, Diffuse: black, Specular: black}
+}
