78e82ac051ee6e92dfdc01185173e5d3122fd746
locatw <locatw@users.noreply.github.com>
2019-03-09 22:33:51 +0900

    move createPixelRays to Camera struct

--- src/github.com/locatw/go-ray-tracer/element/camera.go
+++ src/github.com/locatw/go-ray-tracer/element/camera.go
@@ -1,6 +1,12 @@
 package element
 
-import . "github.com/locatw/go-ray-tracer/vector"
+import (
+	"math"
+	"math/rand"
+
+	"github.com/locatw/go-ray-tracer/image"
+	. "github.com/locatw/go-ray-tracer/vector"
+)
 
 type Camera struct {
 	Origin, Direction, Up Vector
@@ -12,4 +18,38 @@ func CreateCamera(origin Vector, direction Vector, up Vector, fov float64) Camer
 	correctedUp := Normalize(Cross(direction, Cross(up, direction)))
 
 	return Camera{Origin: origin, Direction: correctedDir, Up: correctedUp, Fov: fov}
+}
+
+func (camera *Camera) CreatePixelRays(width int, height int, coord image.Coordinate, samplingCount int) []Ray {
+	aspect := float64(width) / float64(height)
+
+	screenXAxis := Normalize(Cross(camera.Direction, camera.Up))
+	screenYAxis := Multiply(-1.0, camera.Up)
+	screenHeight := 2.0 * math.Tan(camera.Fov/2.0)
+	screenWidth := screenHeight * aspect
+
+	screenCenter := Add(camera.Origin, camera.Direction)
+
+	pixelWidth := screenWidth / float64(width)
+	pixelHeight := screenHeight / float64(height)
+	offset := Multiply(pixelWidth/2.0, screenXAxis)
+	offset = Add(offset, Multiply(pixelHeight/2.0, screenYAxis))
+
+	leftTopPixelCenter := Subtract(screenCenter, Multiply(screenWidth/2.0, screenXAxis))
+	leftTopPixelCenter = Subtract(leftTopPixelCenter, Multiply(screenHeight/2.0, screenYAxis))
+	leftTopPixelCenter = Add(leftTopPixelCenter, offset)
+
+	pixelCenter := Add(leftTopPixelCenter, Multiply(float64(coord.X)*pixelWidth, screenXAxis))
+	pixelCenter = Add(pixelCenter, Multiply(float64(coord.Y)*pixelHeight, screenYAxis))
+
+	rays := make([]Ray, samplingCount)
+	for i := 0; i < samplingCount; i++ {
+		x := rand.Float64() - 0.5
+		y := rand.Float64() - 0.5
+
+		subPixelPos := AddAll(pixelCenter, Multiply(x*pixelWidth, screenXAxis), Multiply(y*pixelHeight, screenYAxis))
+		rays[i] = CreateRay(camera.Origin, Subtract(subPixelPos, camera.Origin))
+	}
+
+	return rays
 }
--- src/github.com/locatw/go-ray-tracer/rendering/rendering.go
+++ src/github.com/locatw/go-ray-tracer/rendering/rendering.go
@@ -93,45 +93,11 @@ func traceRay(scene Scene, ray Ray, depth int) image.Color {
 	return image.AddColorAll(emissionColor, diffuseColor, specularColor, refractionColor)
 }
 
-func createPixelRays(camera Camera, width int, height int, coord image.Coordinate, samplingCount int) []Ray {
-	aspect := float64(width) / float64(height)
-
-	screenXAxis := Normalize(Cross(camera.Direction, camera.Up))
-	screenYAxis := Multiply(-1.0, camera.Up)
-	screenHeight := 2.0 * math.Tan(camera.Fov/2.0)
-	screenWidth := screenHeight * aspect
-
-	screenCenter := Add(camera.Origin, camera.Direction)
-
-	pixelWidth := screenWidth / float64(width)
-	pixelHeight := screenHeight / float64(height)
-	offset := Multiply(pixelWidth/2.0, screenXAxis)
-	offset = Add(offset, Multiply(pixelHeight/2.0, screenYAxis))
-
-	leftTopPixelCenter := Subtract(screenCenter, Multiply(screenWidth/2.0, screenXAxis))
-	leftTopPixelCenter = Subtract(leftTopPixelCenter, Multiply(screenHeight/2.0, screenYAxis))
-	leftTopPixelCenter = Add(leftTopPixelCenter, offset)
-
-	pixelCenter := Add(leftTopPixelCenter, Multiply(float64(coord.X)*pixelWidth, screenXAxis))
-	pixelCenter = Add(pixelCenter, Multiply(float64(coord.Y)*pixelHeight, screenYAxis))
-
-	rays := make([]Ray, samplingCount)
-	for i := 0; i < samplingCount; i++ {
-		x := rand.Float64() - 0.5
-		y := rand.Float64() - 0.5
-
-		subPixelPos := AddAll(pixelCenter, Multiply(x*pixelWidth, screenXAxis), Multiply(y*pixelHeight, screenYAxis))
-		rays[i] = CreateRay(camera.Origin, Subtract(subPixelPos, camera.Origin))
-	}
-
-	return rays
-}
-
 func renderPixel(scene Scene, width int, height int, pixel *image.Pixel) {
 	samplingCount := 1000
 
 	pixelColor := image.CreateDefaultColor(image.Black)
-	for _, ray := range createPixelRays(scene.Camera, width, height, pixel.Coordinate, samplingCount) {
+	for _, ray := range scene.Camera.CreatePixelRays(width, height, pixel.Coordinate, samplingCount) {
 		color := traceRay(scene, ray, 10)
 
 		pixelColor = image.AddColor(pixelColor, color)
